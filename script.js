2370c5d2f7b762c4f2dd96dd43247921c1b349b4
Sagar <sagarpal786t@gmail.com>
2025-12-13 06:14:08 +0000

    second commit

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 document.getElementById("prev").addEventListener("click", clickPrev);
 document.getElementById("next").addEventListener("click", clickNext);
 
-
 mainDivison = document.querySelector(".main");
 
 let state = 0;
+
+const prevButton = document.getElementById("prev");
+const nextButton = document.getElementById("next");
 
 function clickPrev() {  
     if(state==0) return;
@@ -13,8 +15,19 @@ function clickPrev() {
 }
 
 function clickNext() {
-    if(state==50) return;
+    if(state==100) return;
     state += 25;
     mainDivison.style.right = state + '%';
 }
 
+let clicked = -1;
+
+const ul = document.querySelector(".nav ul").children;
+for(let i=0; i<ul.length; i++){
+    const a = ul[i].firstChild;
+    a.addEventListener("click", function(){
+        if(clicked != -1) ul[clicked].classList.remove("active");
+        ul[i].classList.add("active");
+        clicked = i;
+    });
+}
