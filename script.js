eb24bf79fe65a37a9cf038794744260c0f0829e8
Sagar <sagarpal786t@gmail.com>
2025-12-19 07:07:30 +0000

    updated changes

--- script.js
+++ script.js
@@ -93,3 +93,98 @@ for(let i=0; i<lis.length; i++) {
     });
 }
 
+
+
+/* Product data coming from outside */
+/* As of now simple data coming from array in coding language */
+
+// let productcards = [
+//     {
+//         "url" : "./images/product-cards/1-1221-vilante-boys-girls-original-imahgd8jv9zcgtxg.webp",
+//         "text" : "Wrist Watches",
+//         "para" : "Min. 50% Off"
+//     },
+//     {
+//         "url" : "./images/product-cards/6-biss-2031-blu-wht-40-bruton-blue-original-imahczyufwkmpeyq.webp",
+//         "text" : "Men's Sports Shoes",
+//         "para" : "Min. 70% Off"
+//     },
+// ]
+
+
+// const cardscontainer = document.querySelector("#elect .cards-container");
+// for(let i=0; i<productcards.length; i++){
+
+//     /*
+//         Step 1 = get product data
+//         step 2 = have cards container location
+//         step 3 = add new div with having classname as card
+//         step 4 = create new img element, span, p
+//         Step 5 = Fill above 3 data with product data
+//         Step 6 = Add 3 elements in card
+//     */
+//     let product = productcards[i];  //Step 1
+//     const card = document.createElement("div"); //step 3
+//     card.classList.add("card"); // step 3
+
+//     const img = document.createElement("img");
+//     img.src = product.url;
+
+//     const span = document.createElement("span");
+//     span.textContent = product.text;
+
+//     const para = document.createElement("p");
+//     para.textContent = product.para;
+
+//     card.appendChild(img);
+//     card.appendChild(span);
+//     card.appendChild(para);
+
+//     cardscontainer.appendChild(card);
+// }
+
+
+
+// $('#ecomCardContainer').each(function (ind, value) {
+
+//     $('.view-product').click(function(){
+//         $("p").hide();
+//     });
+// });
+
+
+
+// $(document).ready(function(){
+//   $('.view-product').click(function(){
+//     this.pa
+//   });
+// });
+
+const productcards = document.getElementById('ecomCardContainer');
+for(let i=0; i<productcards.length; i++){
+    const productcard = productcards[i];
+    const cardbody = productcard.firstChild.lastChild
+    const button = cardbody.lastChild;
+    console.log(productcard);
+    button.addEventListener("click", function(){
+        
+        alert(cardbody.firstChild.textContent);
+    });
+}
+
+$('.view-product-button').click(function(){
+    productname = this.parentElement.firstChild.textContent;
+    alert(productname);
+});
+
+$.get("./products.txt", function(_, _, xhr){
+    if(xhr.status === 200){
+        console.log(xhr.responseText);
+    }else if(xhr.status === 500){
+        console.log("Internal Server Error");
+    }else{
+        console.log("Other error occured");
+    }
+});
+
+
